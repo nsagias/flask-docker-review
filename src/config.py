4facce6586977031d82df4618a987df93a478072
Nick <nsagias@gmail.com>
2022-12-08 01:11:46 -0500

    add config.py file

---
+++ src/config.py
@@ -0,0 +1,15 @@
+# src/config.py
+
+import os
+
+class BaseConfig:
+  TESTING = False
+
+class DevelopmentConfig(BaseConfig):
+  pass
+
+class TestingConfig(BaseConfig):
+  TESTING = True
+  
+class ProductionConfig(BaseConfig):
+  pass
