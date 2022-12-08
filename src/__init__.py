5bcab90818eed764930dc658abbc02fc51492a11
Nick <nsagias@gmail.com>
2022-12-07 23:02:15 -0500

    add app setting

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,6 @@
 # src/__init__.py
+import os
+
 
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
@@ -7,6 +9,10 @@ from flask_restx import Resource, Api
 app = Flask(__name__)
 
 api = Api(app)
+
+# set config
+app_settings = os.getenv('APP_settings')
+app.config.from_object(app_settings)
 
 class Ping(Resource):
   def get(self):
