8d3aab9ebba27a3b59aeb99175b32b8d84f78aff
Nick <nsagias@gmail.com>
2022-12-08 01:36:36 -0500

    add sqlalchemy to app

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,10 @@
 # src/__init__.py
 
+import os
+
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 # instantiate app
 app = Flask(__name__)
@@ -11,6 +14,9 @@ api = Api(app)
 # set config
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
+
+# instantiate the db
+db = SQLAlchemy(app)
 
 class Ping(Resource):
   def get(self):
