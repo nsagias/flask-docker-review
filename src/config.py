361a886a2b263f48199808e65b9d51f1ac07c496
Nick <nsagias@gmail.com>
2022-12-09 02:15:49 -0500

    setup test environement with configs

--- src/config.py
+++ src/config.py
@@ -5,6 +5,7 @@ import os
 class BaseConfig:
   TESTING = False
   SQLALCHEMY_TRACK_MODIFICATIONS = False
+  SECRET_KEY = 'not_very_secret_key'
 
 class DevelopmentConfig(BaseConfig):
   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
