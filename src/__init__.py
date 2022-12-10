e43f0381e7c4c4d32b94a9e18dd4dfff31f68560
Nick <nsagias@gmail.com>
2022-12-10 18:12:25 -0500

    refactor

--- src/__init__.py
+++ src/__init__.py
@@ -3,19 +3,12 @@
 import os
 
 from flask import Flask
-
 from flask_sqlalchemy import SQLAlchemy
 
-# instantiate app
-app = Flask(__name__)
 
-
-# set config
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
 
 # instantiate the db
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 def create_app(script_info=None):
   
