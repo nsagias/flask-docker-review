bc08af7bf1436fc516d7493f94f79bcb088b46b8
Nick <nsagias@gmail.com>
2022-12-10 18:12:58 -0500

    fix typo

--- src/api/models.py
+++ src/api/models.py
@@ -6,7 +6,7 @@ from src import db
 class User(db.Model):
   __tablename__ = 'users'
   
-  id = db.Column(db.Integer, primary=True, autoincrement=True)
+  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
   username = db.Column(db.String(128), nullable=False)
   email = db.Column(db.String(128), nullable=False)
   active = db.Column(db.Boolean(), default=True, nullable=False)
