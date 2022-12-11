7dbff055ef36dcbadcc6205807aa538fc22eefde
Nick <nsagias@gmail.com>
2022-12-11 17:10:01 -0500

    add tests: test_add_user_invalid_json, test_add_user_invalidat_json_keys

--- src/tests/test_users.py
+++ src/tests/test_users.py
@@ -7,12 +7,35 @@ def test_add_user(test_app, test_database):
     resp = client.post(
         '/users',
         data=json.dumps({
-        'username' : 'nick',
-        'email': 'anyemail@anywhere.com'
+            'username' : 'nick',
+            'email': 'anyemail@anywhere.com'
         }),
+        content_type='application/json'
     )
     data = json.loads(resp.data.decode())
     assert resp.status_code == 201
     assert 'anyemail@anywhere.com was added!' in data['message']
     
-  
+
+def test_add_user_invalid_json(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.post(
+        '/users',
+        data=json.dumps({}),
+        content_type='application/json',
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert 'Input payload validation failed' in data['message']
+
+
+def test_add_user_invalid_json_keys(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.post(
+        '/users',
+        data=json.dumps({"email": "any@email.com"}),
+        content_type='application/json',
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert 'Input payload validation failed' in data['message']
