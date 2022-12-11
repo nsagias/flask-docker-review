171152669c754c082a0f188ae59d9f5003b51603
Nick <nsagias@gmail.com>
2022-12-11 18:56:39 -0500

    add seed_db to managepy

--- manage.py
+++ manage.py
@@ -16,5 +16,11 @@ def recreate_db():
     db.create_all()
     db.session.commit()
 
+@cli.command('seed_db')
+def seed_db():
+    db.session.add(User(username='nick1', email='nick1@anyemail.com'))
+    db.session.add(User(username='nick2', email='nick2@anyemail.com'))
+    db.session.commit()
+
 if __name__ == '__main__':
     cli()
