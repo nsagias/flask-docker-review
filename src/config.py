54b37574756faf853151458ebdf35eb27db221a1
Nick <nsagias@gmail.com>
2022-12-18 23:36:30 -0500

    update dockfile format and fix src/config.py

--- src/config.py
+++ src/config.py
@@ -5,14 +5,19 @@ import os
 class BaseConfig:
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'not_very_secret_key'
+    SECRET_KEY = "not_very_secret_key"
 
 class DevelopmentConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") 
 
 class TestingConfig(BaseConfig):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")
   
 class ProductionConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    url1 = os.environ.get("DATABASE_URL")
+    
+    if url1 is not None and url1.startswith("postgres://"):
+        url = url1.replace("postgres://", "postgresql://", 1)
+        
+    SQLALCHEMY_DATABASE_URI = url
