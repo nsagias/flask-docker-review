b87244ae7468cd61f4561040f311d90a21452a43
Nick <nsagias@gmail.com>
2022-12-26 00:52:14 -0500

    create user crud and scaffold crud skeleton

---
+++ src/api/crud.py
@@ -0,0 +1,25 @@
+# src/api/crud.py
+
+from src import db
+from src.api.models import User
+
+def get_all_users():
+    pass
+
+
+def get_users_by_id(user_id):
+    pass
+
+
+def get_user_by_email(email):
+    pass
+
+
+def add_user(username, email):
+    pass
+
+def update_user(user, username, email):
+    pass
+
+def delete_user(user):
+    pass
